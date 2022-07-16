2bcc11f9e9da49e619fde865a2d1d45bf01f3214
diogostahnke <diogostk.br@gmail.com>
2022-07-15 22:17:59 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php 
-session_start();
 //The url you wish to send the POST request to
 $url = "https://api.mercadolibre.com/oauth/token";
 
@@ -33,7 +32,6 @@ $result = curl_exec($ch);
 
 curl_close($ch);
 $decoded = json_decode($result);
-$_SESSION['access_token']($decoded->{'access_token'});
 
 header('Location: index.php')
 
